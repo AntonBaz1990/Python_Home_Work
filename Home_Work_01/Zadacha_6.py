4ae4c8d921d672588dc9ca81a8c80a268d10ab64
simpo5000 <simpo5000@gmail.com>
2022-12-13 00:54:29 +0300

    Решение задачи 6

--- Home_Work_01/Zadacha_6.py
+++ Home_Work_01/Zadacha_6.py
@@ -8,3 +8,48 @@
 # Пример:
 # 385916 -> yes
 # 123456 -> no
+
+def SumElements(num):
+    sum = 0
+    ost = 0
+    while num > 0:
+        ost = num % 10
+        sum += ost
+        num //= 10
+    return sum
+        
+num = int(input("Введите число: "))
+num1 = num // 1000
+num2 = num % 1000
+sum1 = SumElements(num1)
+sum2 = SumElements(num2)
+
+if sum1 == sum2:
+    print("YES")
+else:
+    print("NO")  
+
+
+# ...........Альтернативное решение
+# num = int(input("Введите число: "))
+# num1 = num // 1000
+# num2 = num % 1000
+
+# sum1 = 0
+# ost1 = 0
+# while num1 > 0:
+#     ost1 = num1 % 10
+#     sum1 += ost1
+#     num1 //= 10
+
+# sum2 = 0
+# ost2 = 0
+# while num2 > 0:
+#     ost2 = num2 % 10
+#     sum2 += ost2
+#     num2 //= 10
+
+# if sum1 == sum2:
+#     print("YES")
+# else:
+#     print("NO")
