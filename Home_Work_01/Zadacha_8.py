228f8dd2541a7b7026b57b797bfe8748bfdc82e6
simpo5000 <simpo5000@gmail.com>
2022-12-13 00:12:02 +0300

    Решение задачи 8

--- Home_Work_01/Zadacha_8.py
+++ Home_Work_01/Zadacha_8.py
@@ -9,7 +9,7 @@
 n = int(input("Введите n:"))
 m = int(input("Введите m:"))
 k = int(input("Введите k:"))
-if k < m*n and (k%m==0 or k%n==0):
+if k < m * n and (k  %m == 0 or k % n == 0):
     print('YES')
 else:
     print('NO')
