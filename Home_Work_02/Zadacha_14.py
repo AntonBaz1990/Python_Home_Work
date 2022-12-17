f79eb59b5da5772985f4aae79df0e44ac380d22b
simpo5000 <simpo5000@gmail.com>
2022-12-17 19:35:21 +0300

    Решение задачи 14

--- Home_Work_02/Zadacha_14.py
+++ Home_Work_02/Zadacha_14.py
@@ -2,8 +2,11 @@
 # Требуется вывести все целые степени двойки (т.е. числа вида 2k), не превосходящие числа N.
 # 10 -> 1 2 4 8
 
-nun = int(input("Введите число: "))
-i = 1
-while i <= nun:
-    print(i, end=' ')
-    i = i * 2
+def FuntionSquer(n) :
+    i = 1
+    while i <= n:   
+        print(i, end=' ')
+        i = i * 2
+        
+num = int(input("Введите число: "))
+FuntionSquer(num)
