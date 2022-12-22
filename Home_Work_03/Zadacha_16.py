ad6bc44fe8f2c0d041fb7fb09e5ad9caa40b230b
simpo5000 <simpo5000@gmail.com>
2022-12-22 13:56:55 +0300

    Решение задачи 16

---
+++ Home_Work_03/Zadacha_16.py
@@ -0,0 +1,34 @@
+# Задача 16: Требуется вычислить, сколько раз встречается некоторое число X в массиве A[1..N].
+# Пользователь вводит натуральное число N – количество элементов в массиве.
+# и число, которое необходимо проверить - X.
+
+# 5
+# 1 2 3 4 5
+# 3
+# -> 1
+import os
+os.system('cls')
+
+def RandomArray(n) :  
+    import random
+    m = []
+    eagle = 0 
+    for i in range(0, n):
+        random_num = round(random.randint(0, n//2))
+        m.append(random_num)
+    return m    
+
+def FindNum( num):
+    count = 0
+    i=0
+    for i in array :
+        if i == X:
+            count += 1
+    return count   
+
+N = int(input("Введите количество элементов в массиве: "))
+X = int(input("Введитеискомую цифру: "))
+array = RandomArray(N)
+print(array)
+find_num = FindNum(X)          
+print("Цифра",X , "встречается", find_num, "раз")        
