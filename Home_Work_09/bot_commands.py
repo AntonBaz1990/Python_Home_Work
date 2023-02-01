3672d541f36be52fe44eebc90c5f6a4ad6849136
simpo5000 <simpo5000@gmail.com>
2023-02-01 17:51:46 +0300

    Добавлена задача к семинару №9

--- Home_Work_09/bot_commands.py
+++ Home_Work_09/bot_commands.py
@@ -5,6 +5,10 @@ import datetime
 from spy import *
 
 
+
+
+
+
 async def hi_command(update: Update, context: ContextTypes.DEFAULT_TYPE) :
     log(update, context)
     await update.message.reply_text(f'hi {update.effective_user.first_name}')
@@ -25,5 +29,19 @@ async def sum_command(update: Update, context: ContextTypes.DEFAULT_TYPE) :
     x = int(items[1])
     y = int(items[2])
     await update.message.reply_text(f'{x}+{y} = {x+y}')
+
+
+
+async def start_command(update: Update, context: ContextTypes.DEFAULT_TYPE) :
+    log(update, context)
+    await update.message.reply_text(f'Я приветствую ВАС!!! {update.effective_user.first_name}')
+    await update.message.reply_text(f'/hi\n/time\n/help\n/sum')
+
+
+
+
+
+
+
 
 
--- Home_Work_09/main.py
+++ Home_Work_09/main.py
@@ -10,6 +10,7 @@ app.add_handler(CommandHandler("hi", hi_command))
 app.add_handler(CommandHandler("time", time_command))
 app.add_handler(CommandHandler("help", help_command))
 app.add_handler(CommandHandler("sum", sum_command))
+app.add_handler(CommandHandler("start", start_command))
 
 
 print('server start')
